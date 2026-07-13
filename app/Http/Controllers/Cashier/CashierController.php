789bd1d7247bb5f1d0a84fa7ef5df2e44785496b
dima.moisieienko <dimonmail09@gmail.com>
2026-07-13 23:58:01 +0300

    implemented relationship between SaleDetail and Sale, refactoring

--- app/Http/Controllers/Cashier/CashierController.php
+++ app/Http/Controllers/Cashier/CashierController.php
@@ -5,8 +5,12 @@ namespace App\Http\Controllers\Cashier;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Menu;
+use App\Models\Sale;
+use App\Models\SaleDetail;
 use App\Models\Table;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class CashierController extends Controller
 {
@@ -32,19 +36,20 @@ class CashierController extends Controller
         return $html;
     }
 
-    public function getMenuByCategory($category_id){
+    public function getMenuByCategory($category_id)
+    {
         $menus = Menu::where('category_id', $category_id)->get();
         $html = '';
 
-        foreach($menus as $menu) {
+        foreach ($menus as $menu) {
             $html .= '
             <div class="col-md-3 text-center">
                 <a class="btn btn-outline-secondary btn-menu" data-id="' . $menu->id . '">
                     <img class="img-fluid" src="' . url('/menu_images/' . $menu->image) . '"/>
                     <br>
-                        '.$menu->name.'
+                        ' . $menu->name . '
                     <br>
-                    $'.number_format($menu->price, 2).'
+                    $' . number_format($menu->price, 2) . '
                 </a>
             </div>
             ';
@@ -55,8 +60,76 @@ class CashierController extends Controller
 
     public function orderFood(Request $request)
     {
+        $menu = Menu::find($request->menu_id);
+        $table_id = $request->table_id;
+        $table_name = $request->table_name;
+        $sale = Sale::where('table_id', $table_id)->where('sale_status', 'unpaid')->first();
+
+        if (!$sale) {
+            $user = Auth::user();
+            $sale = new Sale();
+            $sale->table_id = $table_id;
+            $sale->table_name = $table_name;
+            $sale->user_id = $user->id;
+            $sale->user_name = $user->name;
+            $sale->save();
+            $sale_id = $sale->id;
+            $table = Table::find($table_id);
+            $table->status = 'unavailable';
+            $table->save();
+        } else {
+            $sale_id = $sale->id;
+        }
+
+        $sale_detail = new SaleDetail();
+        $sale_detail->sale_id = $sale_id;
+        $sale_detail->menu_id = $menu->id;
+        $sale_detail->menu_name = $menu->name;
+        $sale_detail->menu_price = $menu->price;
+        $sale_detail->quantity = $request->quantity;
+        $sale_detail->save();
+
+        $sale->total_price = $sale->total_price + ($request->quantity * $menu->price);
+        $sale->save();
 
 
+        $html = $this->getSaleDetails($sale_id);
+
+        return $html;
+    }
+
+    private function getSaleDetails($sale_id){
+        $html = '<p>Sale ID: ' . $sale->id . '</p>';
+        $saleDetails = SaleDetail::where('sale_id', $sale_id)->get();
+        $html .= '<div class="table-responsive-md" style="overflow-y:scroll; height: 400px; border: 1px solid #343A40" >">
+            <table class="table table-stripped table-dark">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Menu</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Price</th>
+                <th scope="col">Total</th>
+                <th scope="col">Status</th>
+</tr>
+</thead>
+<tbody>';
+
+        foreach ($saleDetails as $saleDetail) {
+            $html .='
+                <tr>
+                <td>'.$saleDetail->menu_id.'</td>
+                <td>'.$saleDetail->menu_name.'</td>
+                <td>'.$saleDetail->quantity.'</td>
+                <td>'.$saleDetail->menu_price.'</td>
+                <td>'.($saleDetail->menu_price * $saleDetail->quantity).'</td>
+                <td>'.$saleDetail->status.'</td>
+</tr>
+            ';
+        }
+
+        $html .='</tbody></table></div>';
+        return $html;
     }
 
 
